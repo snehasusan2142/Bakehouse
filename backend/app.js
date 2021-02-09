41bb45ecbf71bc6ad6ba27293f296f676a61e0e0
Sneha <sneha.susan2142@gmail.com>
2021-02-09 18:38:25 +0530

    second version

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app= new express();
 const Cakesdata=require('./src/model/cakesdata')
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(cors());
 const allcakesRouter= require('./src/routes/allcakesRoute')(app);
